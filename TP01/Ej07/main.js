a8f484e902e6625e8de09cc0b644e0c2f99a0fc6
Gero Mendy <geromendy@gmail.com>
2020-08-31 17:45:33 -0300

    Resuelto Ejercicio 07.b (y solucionado bug menor)

--- TP01/Ej07/main.js
+++ TP01/Ej07/main.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let ctx = canvas.getContext("2d");
     let new_image = new Image();
     new_image.src = "imgs/Saitama_ok - reduced.png";
+    //new_image.crossOrigin = "Anonymous";
     new_image.onload = () => {
 
         dibujarImagen(new_image);
@@ -16,4 +17,25 @@ document.addEventListener("DOMContentLoaded", () => {
         canvas.height = new_image.height;
         ctx.drawImage(new_image, 0, 0);
     }
+
+    function cambiarAGrises(img) {      //IMPORTANTE! - Necesita ejecutarse desde XAMPP para tener los permisos de edición de la imagen (sino falla el "ctx.getImageData").
+
+        for (let pixel = 0; pixel < img.data.length; pixel += 4) {
+
+        
+	    img.data[pixel] = img.data[pixel+1] = img.data[pixel+2] = (img.data[pixel] + img.data[pixel+1] + img.data[pixel+2])/3;
+
+
+        }
+
+        ctx.putImageData(img, 0, 0);
+
+    }
+
+    document.querySelector("#js-toogle_gris").addEventListener("click", () => {
+
+        cambiarAGrises(ctx.getImageData(0, 0, canvas.clientWidth, canvas.clientHeight));
+
+    });
+
 });
