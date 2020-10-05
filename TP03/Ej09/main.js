0d80d4c28d3067d9bb8748ed475e99a107d79887
Gero Mendy <geromendy@gmail.com>
2020-10-05 16:26:52 -0300

    Resuelto TP3 Ej09

---
+++ TP03/Ej09/main.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    "use strict";
+
+    document.querySelector("#button_progress_1").addEventListener("click", () => {
+        let barra_carga = document.querySelector("#bar_progress_1");
+        mostrar_carga(barra_carga);
+    });
+    document.querySelector("#button_progress_2").addEventListener("click", () => {
+        let barra_carga = document.querySelector("#bar_progress_2");
+        mostrar_carga(barra_carga);
+    });
+
+
+    function mostrar_carga(elem) {
+        let css_class_name = "loading";
+        elem.classList.add(css_class_name);
+        setTimeout(()=>{
+            elem.classList.remove(css_class_name)
+        }, 5000);
+    }
+    
+});
